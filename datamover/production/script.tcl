89bb3323c3dee13299204ee0fc0b7ec8fdf01c84
Jamil J. Weatherbee <weatherbee@krtkl.com>
2018-08-29 11:45:01 -0700

    v1.10 exported - 16-bit AXIS <-> 64-bit AXI, 4MT/buffer, ping-pong buffers, 1394 LUTs, 12 BRAM_18k, CP=5.137ns

--- datamover/production/script.tcl
+++ datamover/production/script.tcl
@@ -4,7 +4,7 @@
 ## Copyright (C) 1986-2018 Xilinx, Inc. All Rights Reserved.
 ############################################################
 open_project datamover
-set_top data_mover
+set_top data_mover_16b64b
 add_files datamover/datamover.cpp
 add_files -tb datamover/testbench.cpp
 open_solution "production"
@@ -15,4 +15,4 @@ set_clock_uncertainty 2.0
 csim_design -clean
 csynth_design
 cosim_design -trace_level port -rtl vhdl -tool xsim
-export_design -flow impl -rtl vhdl -format ip_catalog -description "data mover" -vendor "krtl" -library "snickerdoodle" -version "1.05" -display_name "data mover"
+export_design -flow impl -rtl vhdl -format ip_catalog -description "16-bit AXIS to 64-bit AXI Data Mover" -vendor "krtl" -library "snickerdoodle" -version "1.10" -display_name "16-bit AXIS to 64-bit AXI Data Mover"
--- datamover/vivado_hls.app
+++ datamover/vivado_hls.app
@@ -1,11 +1,11 @@
-<project xmlns="com.autoesl.autopilot.project" name="datamover" top="data_mover">
+<project xmlns="com.autoesl.autopilot.project" name="datamover" top="data_mover_16b64b">
     <includePaths/>
     <libraryPaths/>
     <Simulation>
         <SimFlow name="csim" clean="true" csimMode="0" lastCsimMode="0"/>
     </Simulation>
     <files xmlns="">
-        <file name="../testbench.cpp" sc="0" tb="1" cflags=" "/>
+        <file name="../testbench.cpp" sc="0" tb="1" cflags="  -Wno-unknown-pragmas"/>
         <file name="datamover/datamover.cpp" sc="0" tb="false" cflags=""/>
     </files>
     <solutions xmlns="">
